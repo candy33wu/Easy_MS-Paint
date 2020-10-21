f41beb925fcd105bd28a8cfd645db7ea0f5176ba
candy33wu <50259236+candy33wu@users.noreply.github.com>
2020-10-22 02:56:09 +0800

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,14 +2,11 @@
 {
     partial class Form1
     {
-        /// <summary>
         /// 設計工具所需的變數。
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
         /// 清除任何使用中的資源。
-        /// </summary>
+       
         /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
         protected override void Dispose(bool disposing)
         {
@@ -36,7 +33,7 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.開啟舊黨ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟舊檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.開啟新檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.儲存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.顏色調整ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
